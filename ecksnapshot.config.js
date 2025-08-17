69c9fe048941f36b64bac4476087472bcd386303
Dmytro Surovtsev <xelth.com@gmail.com>
2025-08-17 18:22:10 +0200

    'multiagentOkClear'

--- ecksnapshot.config.js
+++
@@ -1,7 +0,0 @@
-// DEPRECATED: This file is no longer used.
-// All configuration has been moved to setup.json in the project root.
-// Please modify setup.json to customize eck-snapshot behavior.
-
-export default {
-  // This file is kept for backwards compatibility only
-};
